55cbab2d9d4e2644eb5e939b287c4ca920836da2
PneumaCore <99826579+PneumaCore@users.noreply.github.com>
2024-10-15 14:51:49 +0200

    Creación del proyecto

---
+++ android/app/src/main/java/ionic/expensetrack/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.expensetrack;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.expensetrack',
+  appName: 'expensetrack',
+  webDir: 'dist'
+};
+
+export default config;
