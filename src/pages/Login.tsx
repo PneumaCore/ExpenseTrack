ecf65d8ea0268b6ce23a3efd08e694edf22a10b7
PneumaCore <99826579+PneumaCore@users.noreply.github.com>
2024-11-04 12:26:29 +0100

    Ajuste de grid para el formulario de pantalla de login y registro

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -24,7 +24,7 @@ const LogIn: React.FC = () => {
       setLoading(false);
     }
   };
-  
+
   return (
     <IonPage>
       <IonHeader>
@@ -36,17 +36,43 @@ const LogIn: React.FC = () => {
         {loading && <IonLoading isOpen={loading} message={'Iniciando sesión...'} />}
 
         <IonGrid>
+
+          {/* Campo de error de autentificación */}
           <IonRow className="login-row">
             <IonCol size="12" size-md="8" offset-md="2">
               {error && <p className="error-message">{error}</p>}
+            </IonCol>
+          </IonRow>
+
+          {/* Campo de correo electrónico */}
+          <IonRow>
+            <IonCol size="12" size-md="8" offset-md="2">
               <IonItem>
                 <IonInput label='Correo electrónico' labelPlacement='floating' placeholder='Correo electrónico' type="email" value={email} onIonChange={(e) => setEmail(e.detail.value!)} required />
               </IonItem>
+            </IonCol>
+          </IonRow>
+
+          {/* Campo de contraseña */}
+          <IonRow>
+            <IonCol size="12" size-md="8" offset-md="2">
               <IonItem>
                 <IonInput label='Contraseña' labelPlacement='floating' placeholder='Contraseña' type="password" value={password} onIonChange={(e) => setPassword(e.detail.value!)} required />
               </IonItem>
+            </IonCol>
+          </IonRow>
+
+          {/* Botón para iniciar sesión */}
+          <IonRow>
+            <IonCol size="12" size-md="8" offset-md="2">
               <IonButton expand="full" onClick={handleLogIn}>Iniciar Sesión</IonButton>
-              
+            </IonCol>
+          </IonRow>
+
+          {/* Link para formulario de registro */}
+          <IonRow>
+            <IonCol size="12" size-md="8" offset-md="2">
+
               {/* Si el usuario desea registrarse, se le redirige a la página de registro */}
               <IonLabel className="signup-link" onClick={() => history.push('/signup')}>¿Aún no tienes una cuenta? Regístrate</IonLabel>
             </IonCol>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -16,7 +16,7 @@ const SignUp: React.FC = () => {
 
     /* Se registra con el correo y las contraseñas proporcionadas por el usuario, si el registro es exitoso, se le redirige a la pantalla de inicio de sesión */
     const handleSignUp = async () => {
-        
+
         /* Se comprueba que ambas contraseñas coincidan */
         if (password !== repeatPassword) {
             setError('Las contraseñas no coinciden');
@@ -52,18 +52,44 @@ const SignUp: React.FC = () => {
                 {loading && <IonLoading isOpen={loading} message={'Registrándose...'} />}
 
                 <IonGrid>
+
+                    {/* Campo de error de registro */}
                     <IonRow className="signup-row">
                         <IonCol size="12" size-md="8" offset-md="2">
                             {error && <p className="error-message">{error}</p>}
+                        </IonCol>
+                    </IonRow>
+
+                    {/* Campo de correo electrónico */}
+                    <IonRow>
+                        <IonCol size="12" size-md="8" offset-md="2">
                             <IonItem>
                                 <IonInput label='Correo electrónico' labelPlacement='floating' placeholder='Correo electrónico' type="email" value={email} onIonChange={(e) => setEmail(e.detail.value!)} required />
                             </IonItem>
+                        </IonCol>
+                    </IonRow>
+
+                    {/* Campo de contraseña */}
+                    <IonRow>
+                        <IonCol size="12" size-md="8" offset-md="2">
                             <IonItem>
                                 <IonInput label='Contraseña' labelPlacement='floating' placeholder='Contraseña' type="password" value={password} onIonChange={(e) => setPassword(e.detail.value!)} required />
                             </IonItem>
+                        </IonCol>
+                    </IonRow>
+
+                    {/* Campo para repetir contraseña */}
+                    <IonRow>
+                        <IonCol size="12" size-md="8" offset-md="2">
                             <IonItem>
                                 <IonInput label='Repetir contraseña' labelPlacement='floating' placeholder='Repetir contraseña' type="password" value={repeatPassword} onIonChange={(e) => setRepeatPassword(e.detail.value!)} required />
                             </IonItem>
+                        </IonCol>
+                    </IonRow>
+
+                    {/* Botón para registarse */}
+                    <IonRow>
+                        <IonCol size="12" size-md="8" offset-md="2">
                             <IonButton expand="full" onClick={handleSignUp}>Registrarse</IonButton>
                         </IonCol>
                     </IonRow>
