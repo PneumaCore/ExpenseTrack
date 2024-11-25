cd973235e88c8ce28d44cfcf4c80f66759659c16
PneumaCore <99826579+PneumaCore@users.noreply.github.com>
2024-11-25 20:03:51 +0100

    Las categorías asociadas al usuario muestran sus iconos mapeados con FontAwesome

--- src/components/AddCategory.tsx
+++ src/components/AddCategory.tsx
@@ -16,37 +16,37 @@ interface AddCategoryProps {
 const AddCategory: React.FC<AddCategoryProps> = ({ isOpen, onClose }) => {
   const [type, setType] = useState('gasto');
   const [name, setName] = useState('');
-  const [icon, setIcon] = useState(faDollarSign);
+  const [icon, setIcon] = useState(faHome);
   const [color, setColor] = useState('#000000');
 
   const handleSaveCategory = async () => {
-
-    /* Obtenemos los datos del usuario autenticado */
-    const auth = getAuth();
-    const currentUser = auth.currentUser;
-
-    /* Generamos un ID automático con Firestore */
-    const categoriesRef = doc(collection(database, 'categories'));
-    const categoryId = categoriesRef.id;
-
-    const newCategory = {
-      category_id: categoryId,
-      user_id: currentUser?.uid,
-      name: name,
-      type: type,
-      icon: icon.iconName,
-      color: color
-    }
-
-    /* Guardamos la categoría en la base de datos */
     try {
+
+      /* Obtenemos los datos del usuario autenticado */
+      const auth = getAuth();
+      const currentUser = auth.currentUser;
+
+      /* Generamos un ID automático con Firestore */
+      const categoriesRef = doc(collection(database, 'categories'));
+      const categoryId = categoriesRef.id;
+
+      const newCategory = {
+        category_id: categoryId,
+        user_id: currentUser?.uid,
+        name: name,
+        type: type,
+        icon: icon.iconName,
+        color: color
+      }
+
+      /* Guardamos la categoría en la base de datos */
       await setDoc(categoriesRef, newCategory);
     } catch (error) {
       console.error("Error al añadir la categoría: ", error);
     }
 
+    /* Cerramos el modal automáticamente al guardar la categoría */
     onClose();
-
   };
 
   return (
@@ -140,7 +140,7 @@ const AddCategory: React.FC<AddCategoryProps> = ({ isOpen, onClose }) => {
                   <div className="color-picker-container">
                     <IonLabel>Selecciona un color</IonLabel>
                     <div>
-                      {['#ff6347', '#3b82f6', '#34d399', '#f59e0b', '#e11d48', '#6366f1'].map(
+                      {['#ff6347', '#3b82f6', '#34d399', '#f59e0b', '#e11d48', '#6366f1', '#9c27b0', '#4caf50', '#ff9800', '#2196f3', '#f44336', '#9e9e9e', '#00bcd4', '#8bc34a'].map(
                         (colorOption, index) => (
                           <div
                             key={index}
@@ -219,7 +219,7 @@ const AddCategory: React.FC<AddCategoryProps> = ({ isOpen, onClose }) => {
                   <div className="color-picker-container">
                     <IonLabel>Selecciona un color</IonLabel>
                     <div>
-                      {['#ff6347', '#3b82f6', '#34d399', '#f59e0b', '#e11d48', '#6366f1'].map(
+                      {['#ff6347', '#3b82f6', '#34d399', '#f59e0b', '#e11d48', '#6366f1', '#9c27b0', '#4caf50', '#ff9800', '#2196f3', '#f44336', '#9e9e9e', '#00bcd4', '#8bc34a'].map(
                         (colorOption, index) => (
                           <div
                             key={index}
--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,12 +1,14 @@
-import { IonButton, IonCol, IonContent, IonFab, IonFabButton, IonGrid, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from "@ionic/react";
+import { faBook, faBriefcase, faBriefcaseMedical, faBuilding, faBus, faCar, faChalkboardTeacher, faChartBar, faChartLine, faCoins, faCreditCard, faFilm, faGasPump, faGift, faGraduationCap, faHandHoldingHeart, faHandHoldingUsd, faHome, faLaptop, faLightbulb, faMoneyBillWave, faMusic, faPiggyBank, faPills, faPuzzlePiece, faReceipt, faShoppingBag, faShoppingBasket, faShoppingCart, faSyncAlt, faTools, faTrophy, faUserMd, faUtensils, faWrench } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IonButton, IonCol, IonContent, IonFab, IonFabButton, IonGrid, IonHeader, IonIcon, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from "@ionic/react";
+import { getAuth } from "firebase/auth";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
 import { add, chevronBack } from "ionicons/icons";
 import { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import AddCategory from "../components/AddCategory";
-import "./Categories.css";
-import { getAuth } from "firebase/auth";
 import { database } from "../configurations/firebase";
-import { collection, getDocs, onSnapshot, query, where } from "firebase/firestore";
+import "./Categories.css";
 
 interface Category {
     category_id: string,
@@ -24,34 +26,75 @@ const Categories: React.FC = () => {
 
     useEffect(() => {
         const fetchCategories = () => {
+            try {
 
-            /* Obtenemos los datos del usuario autenticado */
-            const auth = getAuth();
-            const currentUser = auth.currentUser;
+                /* Obtenemos los datos del usuario autenticado */
+                const auth = getAuth();
+                const currentUser = auth.currentUser;
 
-            /* Obtenemos las categorías asociadas al usuario autenticado */
-            const categoriesRef = collection(database, 'categories');
-            const q = query(categoriesRef, where('user_id', '==', currentUser?.uid));
+                /* Obtenemos las categorías asociadas al usuario autenticado */
+                const categoriesRef = collection(database, 'categories');
+                const q = query(categoriesRef, where('user_id', '==', currentUser?.uid));
 
-            const unsubscribe = onSnapshot(q, (querySnapshot) => {
-                const fetchedCategories = querySnapshot.docs.map((doc) => ({
-                    ...doc.data(),
-                    category_id: doc.id,
-                })) as Category[];
-
-                setCategories(fetchedCategories);
-            });
-
-            return unsubscribe;
+                const unsubscribe = onSnapshot(q, (querySnapshot) => {
+                    const fetchedCategories = querySnapshot.docs.map((doc) => ({
+                        ...doc.data(),
+                        category_id: doc.id,
+                    })) as Category[];
+                    setCategories(fetchedCategories);
+                });
+                return unsubscribe;
+            } catch (error) {
+                console.error("Error al obtener las categorías: ", error);
+            }
         };
-
         const unsubscribe = fetchCategories();
-
         return () => {
             if (unsubscribe) unsubscribe();
         };
     }, []);
 
+    /* Mapeamos todos los iconos de las categorías, si alguno no existe, se mapea uno por defecto */
+    const getFontAwesomeIcon = (iconName: string) => {
+        const icons: { [key: string]: any } = {
+            'home': faHome,
+            'light-bulb': faLightbulb,
+            'tools': faTools,
+            'gas-pump': faGasPump,
+            'bus': faBus,
+            'wrench': faWrench,
+            'car': faCar,
+            'cart-shopping': faShoppingCart,
+            'utensils': faUtensils,
+            'briefcase-medical': faBriefcaseMedical,
+            'pills': faPills,
+            'user-md': faUserMd,
+            'film': faFilm,
+            'music': faMusic,
+            'puzzle-piece': faPuzzlePiece,
+            'graduation-cap': faGraduationCap,
+            'book': faBook,
+            'chalkboard-teacher': faChalkboardTeacher,
+            'credit-card': faCreditCard,
+            'money-bill-wave': faMoneyBillWave,
+            'piggy-bank': faPiggyBank,
+            'chart-line': faChartLine,
+            'gift': faGift,
+            'hand-holding-heart': faHandHoldingHeart,
+            'shopping-bag': faShoppingBag,
+            'briefcase': faBriefcase,
+            'hand-holding-usd': faHandHoldingUsd,
+            'laptop': faLaptop,
+            'shopping-basket': faShoppingBasket,
+            'coins': faCoins,
+            'chart-bar': faChartBar,
+            'building': faBuilding,
+            'sync-alt': faSyncAlt,
+            'trophy': faTrophy,
+            'receipt': faReceipt
+        };
+        return icons[iconName] || faHome;
+    }
 
     return (
         <IonPage id="main-content">
@@ -66,18 +109,17 @@ const Categories: React.FC = () => {
             <IonContent fullscreen>
                 <IonGrid>
                     <IonRow>
+
+                        {/* Se mapean las categorías, si no hay, se muestra */}
                         {categories.length === 0 ? (
                             <IonCol>
                                 <IonLabel>No hay categorías</IonLabel>
                             </IonCol>
                         ) : (
                             categories.map((category) => (
-                                <IonCol key={category.category_id} size="4" className="category-col">
-                                    <div
-                                        className="category-circle"
-                                        style={{ backgroundColor: category.color }}
-                                    >
-                                        <IonIcon icon={category.icon} className="category-icon" />
+                                <IonCol key={category.category_id} size="3" className="category-col">
+                                    <div className="category-circle" style={{ backgroundColor: category.color }}>
+                                        <FontAwesomeIcon icon={getFontAwesomeIcon(category.icon)} className="category-icon" />
                                     </div>
                                     <IonLabel className="category-label">{category.name}</IonLabel>
                                 </IonCol>
