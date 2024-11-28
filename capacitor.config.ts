310b17bb52d8f197a3b79723354a15bbbcf3e2d5
PneumaCore <99826579+PneumaCore@users.noreply.github.com>
2024-11-28 14:04:42 +0100

    Instalación de paquetes npm para futura splash screen para android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ionic.expensetrack',
   appName: 'ExpenseTrack',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: '#ffffff',
+      androidSplashResourceName: 'splash',
+    }
+  }
 };
 
 export default config;
