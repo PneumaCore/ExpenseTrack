8e17b9bb69ab6f7b97a425a8a144408b43a584fe
PneumaCore <99826579+PneumaCore@users.noreply.github.com>
2024-12-05 22:07:19 +0100

    Las transacciones se muestran en la pantalla de inicio

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,11 +1,143 @@
-import { IonButtons, IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonCol, IonContent, IonFab, IonFabButton, IonGrid, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonMenuButton, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import { add } from 'ionicons/icons';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AddTransaction from '../components/AddTransaction';
 import './Tab1.css';
+import { collection, onSnapshot, query, Timestamp, where } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { database } from '../configurations/firebase';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook, faBriefcase, faBriefcaseMedical, faBuilding, faBus, faCar, faChalkboardTeacher, faChartBar, faChartLine, faCoins, faCreditCard, faFilm, faGasPump, faGift, faGraduationCap, faHandHoldingHeart, faHandHoldingUsd, faHome, faLaptop, faLightbulb, faMoneyBillWave, faMusic, faPiggyBank, faPills, faPuzzlePiece, faReceipt, faShoppingBag, faShoppingBasket, faShoppingCart, faSyncAlt, faTools, faTrophy, faUserMd, faUtensils, faWrench } from '@fortawesome/free-solid-svg-icons';
+
+interface Transaction {
+  transaction_id: string,
+  user_id: string,
+  type: string,
+  category_id: string,
+  account_id: string,
+  amount: number,
+  currency: string,
+  date: Timestamp,
+  note: string,
+  created_at: Timestamp
+}
+
+interface Category {
+  category_id: string,
+  user_id: string,
+  name: string,
+  type: string,
+  icon: string,
+  color: string
+}
 
 const Tab1: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    const fetchTransactions = () => {
+      try {
+
+        /* Obtenemos los datos del usuario autenticado */
+        const auth = getAuth();
+        const currentUser = auth.currentUser;
+
+        /* Obtenemos las categorías asociadas al usuario autenticado */
+        const transactionsRef = collection(database, 'transactions');
+        const q = query(transactionsRef, where('user_id', '==', currentUser?.uid));
+
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+          const fetchedTransactions = querySnapshot.docs.map((doc) => ({
+            ...doc.data(),
+            transaction_id: doc.id,
+          })) as Transaction[];
+          setTransactions(fetchedTransactions);
+        });
+        return unsubscribe;
+
+      } catch (error) {
+        console.error("Error al obtener las transacciones: ", error);
+      }
+    };
+    const unsubscribe = fetchTransactions();
+    return () => {
+      if (unsubscribe) unsubscribe();
+    };
+  }, []);
+
+  useEffect(() => {
+    const fetchCategories = () => {
+      try {
+
+        /* Obtenemos los datos del usuario autenticado */
+        const auth = getAuth();
+        const currentUser = auth.currentUser;
+
+        /* Obtenemos las categorías asociadas al usuario autenticado */
+        const categoriesRef = collection(database, 'categories');
+        const q = query(categoriesRef, where('user_id', '==', currentUser?.uid));
+
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+          const fetchedCategories = querySnapshot.docs.map((doc) => ({
+            ...doc.data(),
+            category_id: doc.id,
+          })) as Category[];
+          setCategories(fetchedCategories);
+        });
+        return unsubscribe;
+      } catch (error) {
+        console.error("Error al obtener las categorías: ", error);
+      }
+    };
+    const unsubscribe = fetchCategories();
+    return () => {
+      if (unsubscribe) unsubscribe();
+    };
+  }, []);
+
+  /* Mapeamos todos los iconos de las categorías, si alguno no existe, se mapea uno por defecto */
+  const getFontAwesomeIcon = (iconName: string) => {
+    const icons: { [key: string]: any } = {
+      'home': faHome,
+      'light-bulb': faLightbulb,
+      'tools': faTools,
+      'gas-pump': faGasPump,
+      'bus': faBus,
+      'wrench': faWrench,
+      'car': faCar,
+      'cart-shopping': faShoppingCart,
+      'utensils': faUtensils,
+      'briefcase-medical': faBriefcaseMedical,
+      'pills': faPills,
+      'user-md': faUserMd,
+      'film': faFilm,
+      'music': faMusic,
+      'puzzle-piece': faPuzzlePiece,
+      'graduation-cap': faGraduationCap,
+      'book': faBook,
+      'chalkboard-teacher': faChalkboardTeacher,
+      'credit-card': faCreditCard,
+      'money-bill-wave': faMoneyBillWave,
+      'piggy-bank': faPiggyBank,
+      'chart-line': faChartLine,
+      'gift': faGift,
+      'hand-holding-heart': faHandHoldingHeart,
+      'shopping-bag': faShoppingBag,
+      'briefcase': faBriefcase,
+      'hand-holding-usd': faHandHoldingUsd,
+      'laptop': faLaptop,
+      'shopping-basket': faShoppingBasket,
+      'coins': faCoins,
+      'chart-bar': faChartBar,
+      'building': faBuilding,
+      'sync-alt': faSyncAlt,
+      'trophy': faTrophy,
+      'receipt': faReceipt
+    };
+    return icons[iconName] || faHome;
+  }
 
   return (
     <IonPage id="main-content">
@@ -14,7 +146,7 @@ const Tab1: React.FC = () => {
           <IonButtons slot="start">
             <IonMenuButton></IonMenuButton>
           </IonButtons>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Transacciones</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -23,6 +155,34 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
+
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonList>
+                {transactions.length === 0 ? (
+                  <IonItem>
+                    <IonLabel>No hay transacciones</IonLabel>
+                  </IonItem>
+                ) : (
+                  transactions.map((transaction) => {
+                    const category = categories.find(cat => cat.category_id === transaction.category_id);
+                    return (
+                      <IonItem key={transaction.transaction_id}>
+                        <div className="transaction-category-circle" slot='start' style={{ backgroundColor: category?.color }}>
+                          <FontAwesomeIcon icon={getFontAwesomeIcon(category?.icon || 'default')} className="transaction-category-icon" />
+                        </div>
+                        <IonLabel> {category?.name} </IonLabel>
+                        <IonLabel slot='end'> {transaction.amount} $</IonLabel>
+                      </IonItem>
+                    );
+                  })
+                )}
+              </IonList>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+
         <IonFab slot="fixed" vertical="bottom" horizontal="center">
 
           {/* Abrir el modal para añadir transacciones */}
