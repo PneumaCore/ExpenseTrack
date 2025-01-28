86f13002e88419cdd79c44ddb955f96b42a690fc
PneumaCore <99826579+PneumaCore@users.noreply.github.com>
2025-01-28 23:36:15 +0100

    Formularios de inicio de sesión y registro validados

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,43 @@
-import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonLoading, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAlert, IonButton, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonLoading, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { eye, eyeOff } from 'ionicons/icons';
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
 import { auth, database } from '../configurations/firebase';
 import './LogIn.css';
 
 const LogIn: React.FC = () => {
+  const history = useHistory();
+  const [isTouched, setIsTouched] = useState(false);
+  const [isValid, setIsValid] = useState<boolean>();
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
-  const history = useHistory();
+  const [showAlert, setShowAlert] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  /* Comprobamos que el email sea válido */
+  const validateEmail = (email: string) => {
+    return email.match(
+      /^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    );
+  };
+
+  const validate = (event: Event) => {
+    const value = (event.target as HTMLInputElement).value;
+
+    setIsValid(undefined);
+
+    if (value === '') return;
+
+    validateEmail(value) !== null ? setIsValid(true) : setIsValid(false);
+  };
+
+  const markTouched = () => {
+    setIsTouched(true);
+  };
 
   /* Se inicia sesión con el correo y la contraseña proporcionados por el usuario, si el usuario existe en Firebase, se le redirige al menú principal de la aplicación */
   const handleLogIn = async () => {
@@ -29,7 +55,7 @@ const LogIn: React.FC = () => {
 
         /* Si el usuario ha realizado el formulario inicial, se le redirige a la página principal de la aplicación, si no, se le redirige al formulario inicial */
         if (userData.isAccountSetup) {
-          history.push('/tab1');
+          history.push('/home');
         } else {
           history.push('/account_setup');
         }
@@ -40,7 +66,8 @@ const LogIn: React.FC = () => {
         history.push('/account_setup');
       }
     } catch (err: any) {
-      setError(err.message);
+      setError("El correo electrónico o la contraseña son inválidos");
+      setShowAlert(true);
     } finally {
       setLoading(false);
     }
@@ -53,23 +80,17 @@ const LogIn: React.FC = () => {
           <IonTitle>Iniciar sesión</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="login-content">
+      <IonContent>
         {loading && <IonLoading isOpen={loading} message={'Iniciando sesión...'} />}
+        {showAlert && (<IonAlert isOpen={showAlert} onDidDismiss={() => setShowAlert(false)} header={'Inicio de sesión inválido'} message={error} buttons={['Aceptar']} />)}
 
         <IonGrid>
-
-          {/* Campo de error de autentificación */}
-          <IonRow className="login-row">
-            <IonCol size="12" size-md="8" offset-md="2">
-              {error && <p className="error-message">{error}</p>}
-            </IonCol>
-          </IonRow>
 
           {/* Campo de correo electrónico */}
           <IonRow>
             <IonCol size="12" size-md="8" offset-md="2">
               <IonItem>
-                <IonInput label='Correo electrónico' labelPlacement='floating' placeholder='Correo electrónico' type="email" value={email} onIonChange={(e) => setEmail(e.detail.value!)} required />
+                <IonInput className={`${isValid && 'ion-valid'} ${isValid === false && 'ion-invalid'} ${isTouched && 'ion-touched'}`} label='Correo electrónico' labelPlacement='floating' placeholder='Correo electrónico' type="email" value={email} onIonInput={(e) => { setEmail(e.detail.value!); validate(e); }} onIonBlur={() => markTouched()} required />
               </IonItem>
             </IonCol>
           </IonRow>
@@ -78,7 +99,10 @@ const LogIn: React.FC = () => {
           <IonRow>
             <IonCol size="12" size-md="8" offset-md="2">
               <IonItem>
-                <IonInput label='Contraseña' labelPlacement='floating' placeholder='Contraseña' type="password" value={password} onIonChange={(e) => setPassword(e.detail.value!)} required />
+                <IonInput label='Contraseña' labelPlacement='floating' placeholder='Contraseña' type={showPassword ? "text" : "password"} value={password} onIonInput={(e) => setPassword(e.detail.value!)} required />
+                <IonButton slot="end" fill="clear" onClick={() => setShowPassword(!showPassword)}>
+                  <IonIcon icon={showPassword ? eye : eyeOff} />
+                </IonButton>
               </IonItem>
             </IonCol>
           </IonRow>
@@ -86,7 +110,7 @@ const LogIn: React.FC = () => {
           {/* Botón para iniciar sesión */}
           <IonRow>
             <IonCol size="12" size-md="8" offset-md="2">
-              <IonButton expand="full" onClick={handleLogIn}>Iniciar Sesión</IonButton>
+              <IonButton className='login-button' expand='full' color="medium" shape='round' onClick={handleLogIn}>Iniciar Sesión</IonButton>
             </IonCol>
           </IonRow>
 
@@ -95,12 +119,12 @@ const LogIn: React.FC = () => {
             <IonCol size="12" size-md="8" offset-md="2">
 
               {/* Si el usuario desea registrarse, se le redirige a la página de registro */}
-              <IonLabel className="signup-link" onClick={() => history.push('/signup')}>¿Aún no tienes una cuenta? Regístrate</IonLabel>
+              <IonLabel className="login-link" onClick={() => history.push('/signup')}>¿Aún no tienes una cuenta? Regístrate</IonLabel>
             </IonCol>
           </IonRow>
         </IonGrid>
       </IonContent>
-    </IonPage>
+    </IonPage >
   );
 };
 
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,18 +1,44 @@
 import { useState } from 'react';
 import './SignUp.css';
 import { useHistory } from 'react-router';
-import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonLoading, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonIcon } from '@ionic/react';
-import { chevronBack } from 'ionicons/icons';
+import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonLoading, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonIcon, IonAlert } from '@ionic/react';
+import { chevronBack, eye, eyeOff } from 'ionicons/icons';
 import { createUserWithEmailAndPassword, signOut } from 'firebase/auth';
 import { auth } from '../configurations/firebase';
 
 const SignUp: React.FC = () => {
+    const history = useHistory();
+    const [isTouched, setIsTouched] = useState(false);
+    const [isValid, setIsValid] = useState<boolean>();
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [repeatPassword, setRepeatPassword] = useState<string>('');
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string>('');
-    const history = useHistory();
+    const [showAlert, setShowAlert] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const [showRepeatPassword, setShowRepeatPassword] = useState(false);
+
+    /* Comprobamos que el email sea válido */
+    const validateEmail = (email: string) => {
+        return email.match(
+            /^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+        );
+    };
+
+    const validate = (event: Event) => {
+        const value = (event.target as HTMLInputElement).value;
+
+        setIsValid(undefined);
+
+        if (value === '') return;
+
+        validateEmail(value) !== null ? setIsValid(true) : setIsValid(false);
+    };
+
+    const markTouched = () => {
+        setIsTouched(true);
+    };
 
     /* Se registra con el correo y las contraseñas proporcionadas por el usuario, si el registro es exitoso, se le redirige a la pantalla de inicio de sesión */
     const handleSignUp = async () => {
@@ -20,11 +46,11 @@ const SignUp: React.FC = () => {
         /* Se comprueba que ambas contraseñas coincidan */
         if (password !== repeatPassword) {
             setError('Las contraseñas no coinciden');
+            setShowAlert(true);
             return;
         }
 
         setLoading(true);
-        setError('');
 
         try {
             await createUserWithEmailAndPassword(auth, email, password);
@@ -34,7 +60,8 @@ const SignUp: React.FC = () => {
 
             history.push('/login');
         } catch (err: any) {
-            setError(err.message);
+            setError("El correo electrónico o las contraseñas son inválidas");
+            setShowAlert(true);
         } finally {
             setLoading(false);
         }
@@ -54,21 +81,15 @@ const SignUp: React.FC = () => {
             </IonHeader>
             <IonContent className="signup-content">
                 {loading && <IonLoading isOpen={loading} message={'Registrándose...'} />}
+                {showAlert && (<IonAlert isOpen={showAlert} onDidDismiss={() => setShowAlert(false)} header={'Registro inválido'} message={error} buttons={['Aceptar']} />)}
 
                 <IonGrid>
-
-                    {/* Campo de error de registro */}
-                    <IonRow className="signup-row">
-                        <IonCol size="12" size-md="8" offset-md="2">
-                            {error && <p className="error-message">{error}</p>}
-                        </IonCol>
-                    </IonRow>
 
                     {/* Campo de correo electrónico */}
                     <IonRow>
                         <IonCol size="12" size-md="8" offset-md="2">
                             <IonItem>
-                                <IonInput label='Correo electrónico' labelPlacement='floating' placeholder='Correo electrónico' type="email" value={email} onIonChange={(e) => setEmail(e.detail.value!)} required />
+                                <IonInput className={`${isValid && 'ion-valid'} ${isValid === false && 'ion-invalid'} ${isTouched && 'ion-touched'}`} label='Correo electrónico' labelPlacement='floating' placeholder='Correo electrónico' type="email" value={email} onIonInput={(e) => { setEmail(e.detail.value!); validate(e); }} onIonBlur={() => markTouched()} required />
                             </IonItem>
                         </IonCol>
                     </IonRow>
@@ -77,7 +98,10 @@ const SignUp: React.FC = () => {
                     <IonRow>
                         <IonCol size="12" size-md="8" offset-md="2">
                             <IonItem>
-                                <IonInput label='Contraseña' labelPlacement='floating' placeholder='Contraseña' type="password" value={password} onIonChange={(e) => setPassword(e.detail.value!)} required />
+                                <IonInput label='Contraseña' labelPlacement='floating' placeholder='Contraseña' type={showPassword ? "text" : "password"} value={password} onIonInput={(e) => setPassword(e.detail.value!)} required />
+                                <IonButton slot="end" fill="clear" onClick={() => setShowPassword(!showPassword)}>
+                                    <IonIcon icon={showPassword ? eye : eyeOff} />
+                                </IonButton>
                             </IonItem>
                         </IonCol>
                     </IonRow>
@@ -86,7 +110,10 @@ const SignUp: React.FC = () => {
                     <IonRow>
                         <IonCol size="12" size-md="8" offset-md="2">
                             <IonItem>
-                                <IonInput label='Repetir contraseña' labelPlacement='floating' placeholder='Repetir contraseña' type="password" value={repeatPassword} onIonChange={(e) => setRepeatPassword(e.detail.value!)} required />
+                                <IonInput label='Repetir contraseña' labelPlacement='floating' placeholder='Repetir contraseña' type={showRepeatPassword ? "text" : "password"} value={repeatPassword} onIonInput={(e) => setRepeatPassword(e.detail.value!)} required />
+                                <IonButton slot="end" fill="clear" onClick={() => setShowRepeatPassword(!showRepeatPassword)}>
+                                    <IonIcon icon={showRepeatPassword ? eye : eyeOff} />
+                                </IonButton>
                             </IonItem>
                         </IonCol>
                     </IonRow>
@@ -94,7 +121,7 @@ const SignUp: React.FC = () => {
                     {/* Botón para registarse */}
                     <IonRow>
                         <IonCol size="12" size-md="8" offset-md="2">
-                            <IonButton expand="full" onClick={handleSignUp}>Registrarse</IonButton>
+                            <IonButton className='signup-button' expand="full" color="medium" shape='round' onClick={handleSignUp}>Registrarse</IonButton>
                         </IonCol>
                     </IonRow>
                 </IonGrid>
