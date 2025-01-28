e4c1c6e3e375ac5307ff626d4ff2a92c1704dc4d
PneumaCore <99826579+PneumaCore@users.noreply.github.com>
2025-01-28 19:01:01 +0100

    Pantalla de ajustes

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,7 @@
-import { IonButtons, IonContent, IonGrid, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonContent, IonGrid, IonHeader, IonItem, IonLabel, IonList, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Settings.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCommentDollar, faDatabase, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Settings: React.FC = () => {
     return (
@@ -14,7 +16,26 @@ const Settings: React.FC = () => {
             </IonHeader>
             <IonContent fullscreen>
                 <IonGrid>
-
+                    <IonList className='settings-list'>
+                        <IonItem>
+                            <div slot="start">
+                                <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
+                            </div>
+                            <IonLabel>Perfil</IonLabel>
+                        </IonItem>
+                        <IonItem>
+                            <div slot="start">
+                                <FontAwesomeIcon icon={faDatabase}></FontAwesomeIcon>
+                            </div>
+                            <IonLabel>Datos</IonLabel>
+                        </IonItem>
+                        <IonItem>
+                            <div slot="start">
+                                <FontAwesomeIcon icon={faCommentDollar}></FontAwesomeIcon>
+                            </div>
+                            <IonLabel>Divisa</IonLabel>
+                        </IonItem>
+                    </IonList>
                 </IonGrid>
             </IonContent>
         </IonPage>
